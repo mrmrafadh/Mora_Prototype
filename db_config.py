514d8a974981a67fb0b1f75b9b66a68dadddc329
rafadh <mrmrafadh@gmail.com>
2025-09-23 10:03:39 +0530

    23 2

--- db_config.py
+++ db_config.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import psycopg2
 
 # Function to establish MySQL connection
 def db_conn():
@@ -7,5 +8,17 @@ def db_conn():
         user="root",
         password="root",
         database="foodstation"
+    )
+    return conn
+
+
+
+def db_conn():
+    conn = psycopg2.connect(
+        host="moradb.c38maw0agkjw.ap-south-1.rds.amazonaws.com",
+        port=5432,          # PostgreSQL default port
+        user="postgres",
+        password="rootroot",
+        dbname="foodstation"
     )
     return conn
